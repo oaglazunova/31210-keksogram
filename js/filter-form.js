264b1c528ed82e3271e4cb300bfcb783da6007d8
Olga <oaglaazunova@gmail.com>
2015-09-24 18:44:39 +0300

    New code bits put into correct script files. Cookies are ok. Resize form must be reworked

--- js/filter-form.js
+++ js/filter-form.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   var uploadForm = document.forms['upload-select-image'];
   var resizeForm = document.forms['upload-resize'];
   var filterForm = document.forms['upload-filter'];
@@ -8,6 +8,20 @@
   var selectedFilter = filterForm['upload-filter'];
 
   var filterMap;
+
+  /* restore from cookies */
+  restoreFormValueFromCookies = function (form) {
+    var element;
+    for (i = 0; i < form.elements.length; i++) {
+      element = form.elements[i];
+      if (docCookies.hasItem(element.id)) {
+        element.checked = docCookies.getItem(element.id);
+        previewImage.className = 'filter-image-preview' + ' ' + filterMap[selectedFilter.value];
+      }
+    }
+  };
+  /* restore from cookies end */
+
 
   function setFilter() {
     if (!filterMap) {
@@ -22,12 +36,12 @@
   };
 
   for (var i = 0, l = selectedFilter.length; i < l; i++) {
-    selectedFilter[i].onchange = function(evt) {
+    selectedFilter[i].onchange = function (evt) {
       setFilter();
     }
   }
 
-  prevButton.onclick = function(evt) {
+  prevButton.onclick = function (evt) {
     evt.preventDefault();
 
     filterForm.reset();
@@ -35,12 +49,30 @@
     resizeForm.classList.remove('invisible');
   };
 
-  filterForm.onsubmit = function() {
+  filterForm.onsubmit = function (evt) {
     evt.preventDefault();
+
+    /* save filter type to cookies */
+    var element;
+    for (i = 0; i < filterForm.elements.length; i++) {
+      element = filterForm.elements[i];
+      if (element.name == "upload-filter" && element.checked == true) {
+        docCookies.setItem(element.id, "checked");
+        console.log(element.id, "checked");
+      } else {
+        docCookies.setItem(element.id, "", -1);
+      }
+    }
+    filterForm.submit();
+
+    /* save filter type to cookies end */
 
     uploadForm.classList.remove('invisible');
     filterForm.classList.add('invisible');
   }
 
   setFilter();
+
+  restoreFormValueFromCookies(filterForm); //restore filter type from cookies 
+
 })();
--- js/resize-form.js
+++ js/resize-form.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   var uploadForm = document.forms['upload-select-image'];
   var resizeForm = document.forms['upload-resize'];
   var filterForm = document.forms['upload-filter'];
@@ -6,7 +6,7 @@
   var previewImage = resizeForm.querySelector('.resize-image-preview');
   var prevButton = resizeForm['resize-prev'];
 
-  prevButton.onclick = function(evt) {
+  prevButton.onclick = function (evt) {
     evt.preventDefault();
 
     resizeForm.reset();
@@ -15,11 +15,73 @@
     uploadForm.classList.remove('invisible');
   };
 
-  resizeForm.onsubmit = function(evt) {
+  /* My code */
+  var moveX = resizeForm["resize-x"];
+  var moveY = resizeForm["resize-y"];
+  var sideSize = resizeForm["resize-size"];
+
+  moveX.value = 0;
+  moveY.value = 0;
+ // moveX.min = 0;
+//  moveY.min = 0;
+
+ // var img = document.getElementById('resize-image-preview');
+  var img = document.getElementById('resize-image-preview');
+  var imgWidth; // width of the uploaded image, in px
+  var imgHeight; // height of the uploaded image, in px
+
+  /* find img dimensions, minSideSize, sideSize.value, sideSize.max */
+  img.onload = function (evt) {
+    imgWidth = img.width;
+    imgHeight = img.height;
+
+    console.log(imgWidth, imgHeight);
+
+    /* to handle situation when uploaded image is small -  */
+    if (imgWidth >= 500 && imgHeight >= 500) {
+      sideSize.min = 182; //default min side length of the cropped area (based on div.pictures.picture images), in px
+      sideSize.value = 500;
+    } else if (imgWidth < 500 || imgHeight < 500) {
+      if (imgWidth >= 182 || imgHeight >= 182) {
+        sideSize.min = 182;
+        sideSize.value = 182;
+      } else {
+        if (imgWidth > imgHeight) {
+          sideSize.min = imgHeight;
+          sideSize.value = imgHeight;
+        } else {
+          sideSize.min = imgWidth;
+          sideSize.value = imgWidth;
+        }
+      }
+    }
+    /* to handle situation when uploaded image is small end */
+
+    if (imgHeight > imgWidth) {
+      sideSize.max = imgWidth;
+    } else {
+      sideSize.max = imgHeight;
+    }
+
+  };
+  /* find img dimensions, minSideSize, sideSize.value, sideSize.max end */
+
+  resizeForm.onchange = function (evt) {
+    moveX.max = imgWidth - parseInt(sideSize.value, 10);
+    moveY.max = imgHeight - parseInt(sideSize.value, 10);
+  };
+
+  /* My code end */
+
+
+  resizeForm.onsubmit = function (evt) {
     evt.preventDefault();
     filterForm.elements['filter-image-src'] = previewImage.src;
 
     resizeForm.classList.add('invisible');
     filterForm.classList.remove('invisible');
   };
+
+
+
 })();
