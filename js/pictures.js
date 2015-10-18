66cff248d3cea58a77f9e772346139dddec6730b
Olga <oaglaazunova@gmail.com>
2015-10-18 21:58:54 +0300

    6-1 done (with 2 issues)

--- js/pictures.js
+++ js/pictures.js
@@ -1,6 +1,3 @@
-/* global Photo: true */
-// Gallery: true
-
 'use strict';
 
 (function() {
@@ -24,7 +21,8 @@
   hideFilters();
   */
 
-  // var PICTURE_SIDE_LENGTH = '182px';
+  /* Generate from template */
+ // var PICTURE_SIDE_LENGTH = '182px';
   var REQUEST_FAILURE_TIMEOUT = 10000;
   var PAGE_SIZE = 12;
   var IMG_LOAD_TIMEOUT = 300;
@@ -33,45 +31,39 @@
   var picturesContainer = document.querySelector('.pictures');
   var picturesToRender;
   var filteredPictures;
-  // var Gallery = new Gallery();
-  var renderedPictures = [];
+
+  var Photo;
 
   /* Перепишите функцию вывода списка фотографий таким образом, чтобы она отрисовывала не все доступные изображения, а постранично:
 Каждая страница состоит максимум из 12 фотографий (последняя может содержать меньше).
 Сделайте так, чтобы функция могла работать в двух режимах: добавления страницы и перезаписи содержимого контейнера. */
-  function renderPictures(itemsToRender, pageNumber, replace) {
+  function renderPictures(items, pageNumber, replace) {
     replace = typeof replace !== 'undefined' ? replace : true; // тернарный оператор: условие ? если выполняется : если не выполняется
     pageNumber = pageNumber || 0; // нормализация аргумента
     IMG_LOAD_TIMEOUT = 300;
 
-    // Удаление списка отелей. Пока в массиве renderedHotels есть объекты
-    // Hotel, вызывается функция Array.prototype.shift(), которая удаляет
-    // первый элемент из массива и возвращает его, и у этого отеля вызывается
-    // метод Hotel.prototype.unrender.
     if (replace) {
-      var el;
-      while ((el = renderedPictures.shift())) {
-        el.unrender();
-      }
       picturesContainer.classList.remove('picture-load-failure');
+      picturesContainer.innerHTML = '';
     }
 
+    // var pictureTemplate = document.getElementById('picture-template');
     var picturesFragment = document.createDocumentFragment();
     var picturesFrom = pageNumber * PAGE_SIZE;
     var picturesTo = picturesFrom + PAGE_SIZE;
-    picturesToRender = picturesToRender.slice(picturesFrom, picturesTo);
 
-    // Отрисовка списка отелей. На каждой итерации цикла создается объект
-    // типа Photo с уникальными данными, отрисовывается в предназначенный
-    // для него контейнер (picturesFragment) и добавляется в массив picturesFragment.
-    picturesToRender.forEach(function(pictureData) {
+    items = items.slice(picturesFrom, picturesTo); // pictures =
+
+    items.forEach(function(pictureData) {
       var newPictureElement = new Photo(pictureData);
+
       newPictureElement.render(picturesFragment);
-      renderedPictures.push(newPictureElement);
     });
 
     picturesContainer.appendChild(picturesFragment);
   }
+
+  /* Generate from template end*/
 
   /* Если загрузка закончится неудачно (ошибкой сервера или таймаутом), покажите предупреждение об ошибке, добавив блоку .pictures класс pictures-failure */
   function showLoadFailure() {
@@ -149,15 +141,14 @@
           return b.comments - a.comments; // Для числового сравнения, вместо строкового, функция сравнения может просто вычитать b из a.
         });
         break;
-   /* default:
-      filteredPictures = filteredPictures.sort(function (a, b) {
-        if (typeof a.likes !== 'number') {
-          return 1; // если значение нечисловое, элемент идёт в конец. Для ост. случаев действуех схема для числового сравнения.
-        }
-        return b.likes - a.likes; // Для числового сравнения, вместо строкового, функция сравнения может просто вычитать b из a.
-      });
-      break;
-      */
+      default:
+        filteredPictures = filteredPictures.sort(function(a, b) {
+          if (typeof a.likes !== 'number') {
+            return 1; // если значение нечисловое, элемент идёт в конец. Для ост. случаев действуех схема для числового сравнения.
+          }
+          return b.likes - a.likes; // Для числового сравнения, вместо строкового, функция сравнения может просто вычитать b из a.
+        });
+        break;
     }
 
     localStorage.setItem('filterID', filterID);
@@ -165,49 +156,38 @@
     return filteredPictures;
   }
 
-  // Вызывает функцию фильтрации на списке отелей с переданным fitlerID и подсвечивает кнопку активного фильтра.
-  function setActiveFilter(filterID) {
+  /* Напишите обработчики... end */
+
+  /* После переключения фильтра, должна показываться первая страница (тут-то вам и пригодится режим перезаписи контейнера в функции вывода фотографий).
+После переключения фильтра, выбранное значение должно сохраняться в localStorage и использоваться как значение по умолчанию при следующей загрузке. */
+
+  function setActiveFilter(filterID) { // function setActiveFilter(picturesToRender, filterID) {
     filteredPictures = filterPictures(picturesToRender, filterID);
     currentPage = 0;
 
     renderPictures(filteredPictures, currentPage, true);
-    // document.querySelector('.hotel-filter-selected').classList.remove('hotel-filter-selected');
-    // document.querySelector('#' + filterID).classList.add('hotel-filter-selected');
   }
 
-    /* После переключения фильтра, должна показываться первая страница (тут-то вам и пригодится режим перезаписи контейнера в функции вывода фотографий).
-После переключения фильтра, выбранное значение должно сохраняться в localStorage и использоваться как значение по умолчанию при следующей загрузке. */
-  /* Инициализация подписки на клики по кнопкам фильтра.
-+   * Используется делегирование событий: события обрабатываются на объекте,
-+   * содержащем все фильтры, и в момент наступления события, проверяется,
-+   * произошел ли клик по фильтру или нет и если да, то вызывается функция
-+   * установки фильтра. */
   function initFilters() {
     var filtersContainer = document.querySelector('.filters');
 
     filtersContainer.addEventListener('click', function(evt) {
       var clickedFilter = evt.target;
       setActiveFilter(clickedFilter.id);
-      /*
-      if (doesHaveParent(clickedFilter, 'filters')) {
-        setActiveFilter(clickedFilter.id);
-      }
-    */
     });
   }
 
-  // Проверяет можно ли отрисовать следующую страницу списка
-  function isNextPageAvailable() {
-    return currentPage < Math.ceil(filteredPictures.length / PAGE_SIZE);
-  }
-
-  // Проверяет, находится ли скролл внизу страницы.
+/* Добавьте новый обработчик кастомного события «достижения низа страницы», который будет отображать следующую страницу. */
   function isAtTheBottom() {
     var GAP = 100;
     return picturesContainer.getBoundingClientRect().bottom - GAP <= window.innerHeight;
   }
+  /* */
 
-  // Испускает на объекте window событие loadneeded если скролл находится внизу cтраницы и существует возможность показать еще одну страницу.
+  function isNextPageAvailable() {
+    return currentPage < Math.ceil(filteredPictures.length / PAGE_SIZE); // return currentPage < Math.ceil(pictures.length / PAGE_SIZE);
+  }
+
   function checkNextPage() {
     if (isAtTheBottom() && isNextPageAvailable()) {
       window.dispatchEvent(new CustomEvent('loadneeded'));
@@ -228,34 +208,15 @@
       scrollTimeout = setTimeout(checkNextPage, 100); // throttle - функция вызывается раз в 100 мс
     });
   }
-
-  /**
-   * Добавляет обработчик события showgallery, которое испускается объектом window
-   * в объекте типа Hotel, если произошел клик по фотографии отеля. При наступлении
-   * этого события, показывает фотогалерею и загружает в нее фотографии отеля,
-   * полученные через метод getPhotos() у отеля, переданного через объект
-   * CustomEvent.prototype.detail.
-   */
-    /*
-  function initGallery() {
-    window.addEventListener('showgallery', function (evt) {
-      gallery.setPhotos(evt.detail.pictureElement.getPhotos());
-      gallery.show();
-    });
-  }
-    */
+  /* */
 
   initFilters();
   initScroll();
-  // initGallery();
 
-  // Загружает список отелей из файла data/hotels.json и отрисовывает его
-  // на странице после вызова фильтрации с ID сохраненным в localStorage.
-  // Если в localStorage не сохранен ID фильтра, используется значение
-  // по умолчанию.
-  loadPictures(function(loadedPictures) {
+  loadPictures(function(loadedPictures) { // loadPictures(function(picturesToRender, loadedPictures) {
     picturesToRender = loadedPictures;
-    setActiveFilter(localStorage.getItem('filterID') || 'filter-popular');
+    setActiveFilter(localStorage.getItem('filterID') || 'filter-popular'); // setActiveFilter('filter-popular');
   });
+  /* Загрузите данные из файла data/pictures.json по XMLHttpRequest end */
 
 })();
